cbb9b70fc6c5e4828e3318b72c6c4bd35eb71e4a
AmirOmazgin <81363766+AmirOmazgin@users.noreply.github.com>
2024-04-19 12:16:41 +0300

    Update functions.js

--- cypress/cypress/support/functions.js
+++ cypress/cypress/support/functions.js
@@ -22,13 +22,7 @@ const generateRandomString = (length) => {
       
   }
 
-//   cy.contains('Open New Account').click();
-//   cy.get('div input.button[value="Open New Account"]')
-//     .should('be.visible')
-//     .then(() => {
-//       cy.wait(500)
-//       cy.get('div input.button[value="Open New Account"]').click({ force: true })
-//     });
+
   
   module.exports = { generateRandomString, openNewAccount };
   
